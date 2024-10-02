2ab8a912266218bb131cd2aefc5c06bf6a29e7f7
GabriellaNascimento <gabriella.silva@dcomp.ufs.br>
2024-10-02 10:59:30 -0300

    classe Main

---
+++ src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // Executa a tela inicial
+        HomeScreen homeScreen = new HomeScreen();
+        homeScreen.setVisible(true);
+    }
+}
