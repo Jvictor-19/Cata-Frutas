29f765cbb29447cd6d98e78ad4e32b1f5ff496fc
Irandi silva <irandi.silva@dcomp.ufs.br>
2024-10-09 20:09:43 -0300

    Uptade abacateiro

--- src/Elementos/ElementosEstáticos/Abacateiro.java
+++ src/Elementos/ElementosEstáticos/Abacateiro.java
@@ -2,8 +2,12 @@ package Elementos.ElementosEstáticos;
 
 import javax.swing.ImageIcon;
 import java.awt.Graphics;
+import java.awt.Image;
 
 public class Abacateiro extends Arvore {
+
+    private int larguraOriginal;  // Largura original da imagem
+    private int alturaOriginal;   // Altura original da imagem
 
     public Abacateiro(int x, int y) {
         super(x, y);
@@ -13,12 +17,25 @@ public class Abacateiro extends Arvore {
     protected void carregarImagem() {
         ImageIcon icone = new ImageIcon("src/imagens/Abacateiro.png");
         imagemArvore = icone.getImage(); // Mantenha a imagem original
+
+        // Captura as dimensões originais da imagem
+        larguraOriginal = imagemArvore.getWidth(null);
+        alturaOriginal = imagemArvore.getHeight(null);
     }
 
     @Override
     public void desenhar(Graphics g, int tamanhoTile) {
-        int largura = 170;  // Defina a largura desejada
-        int altura = 170;   // Defina a altura desejada
-        g.drawImage(imagemArvore, x * tamanhoTile, y * tamanhoTile, largura, altura, null); // Desenhe com dimensões definidas
+        // Calcula a proporção de escala
+        double escala = Math.min((double)tamanhoTile / larguraOriginal, (double)tamanhoTile / alturaOriginal);
+        
+        int largura = (int) (larguraOriginal * escala);
+        int altura = (int) (alturaOriginal * escala);
+
+        // Calcula as coordenadas para centralizar a imagem
+        int posX = x * tamanhoTile + (tamanhoTile - largura) / 2;
+        int posY = y * tamanhoTile + (tamanhoTile - altura) / 2;
+
+        // Desenha a imagem escalada e centralizada
+        g.drawImage(imagemArvore, posX, posY, largura-20, altura-20, null);
     }
 }
